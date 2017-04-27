f6a1030e9193d45ec873f33b0760f1398ec44417
Mifist <daria1992moskalets@gmail.com>
2017-04-28 02:50:15 +0300

    First commit

---
+++ testwoo-plugin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: avant
+ * Date: 28.04.2017
+ * Time: 2:46
+ */
+/**
+ * Plugin Name: TestWoo Plugin
+ * Plugin URI: https://github.com/SolomashenkoRoman/woocommerce-lesson
+ * Description: Woocommerce Lesson
+ * Version: 1.0.0
+ * Author: WooCommerce
+ * Author URI: http://woocommerce.com/
+ * Developer: Solomashenko Roman
+ * Developer URI: https://www.linkedin.com/in/solomashenkoroman/
+ * Text Domain: woocommerce-lesson
+ * Domain Path: /languages
+ *
+ * Copyright: © 2009-2015 WooCommerce.
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ */
